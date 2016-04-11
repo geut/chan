f23bee1ad16e38cb42aab2a990bb7f9d302cb122
tincho <tinchoz49@gmail.com>
2016-04-10 22:35:20 -0300

    added some comments

--- src/cli/commands/added.js
+++ src/cli/commands/added.js
@@ -3,6 +3,14 @@ export default function () {
         command: 'added <msg>',
         description: 'Writes your changelog indicating new stuff.',
         action(parser, msg) {
+            /**
+             * podes modificar el changelog usando parser.root y recorriendo
+             * los nodos y cambiando su "value". Despues automaticamente se
+             * va guardar la nueva version del changelog.
+             *
+             * OJO con probarlo con el changelog del proyecto chan, tendriamos
+             * que arrancar a hacer tests.
+             */
             console.log('ADDED: ' + msg);
         }
     };
