fe8187b4f5eb8471caee66e02199835a1777a92a
tincho <tinchoz49@gmail.com>
2016-04-12 21:19:56 -0300

    fix structure for tests

--- src/cli/commands/init.js
+++ src/cli/commands/init.js
@@ -2,7 +2,7 @@ export default function () {
     return {
         command: 'init',
         description: 'Init the document',
-        action(parser) {
+        action() {
             // console.log(parser.root);
         }
     };
---
+++ test/commands/init.js
@@ -0,0 +1,8 @@
+import test from 'tape';
+import tempfile from 'tempfile';
+
+test('test "init" command', function (t) {
+    t.plan(1);
+
+    t.equal('pepe', 'pepe', 'should pass');
+});
