37f8b37c0db374e3e27254ecb4d9f8089d22099d
Diego Paez <paez.di@gmail.com>
2016-06-02 21:12:41 -0300

    Update: wip test init

--- test/commands/init.js
+++ test/commands/init.js
@@ -1,8 +1,20 @@
 import test from 'tape';
+import proxyquire from 'tape';
+import cli from '../../src/cli';
 // import tempfile from 'tempfile';
+
+// const stubs = {
+//   'inquirer': {
+//     prompt: new Prompt(this.fixture, this.rl),
+//     '@global': true
+//   }
+// };
+
+const originalArgv = process.argv;
 
 test('test "init" command', (t) => {
     t.plan(1);
-
+    process.argv.push( 'chan', '--path', '/tmp/chan_test/', 'init' );
+    cli.run();
     t.pass('example');
 });
