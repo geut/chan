0d1ccfcad1a4ea316de484286690f39c08e517eb
Diego Paez <paez.di@gmail.com>
2016-06-02 21:12:12 -0300

    Update: parser code organization

--- src/parser/index.js
+++ src/parser/index.js
@@ -1,22 +1,42 @@
+import fs from 'fs';
 import path from 'path';
 import remark from 'remark';
-import fs from 'fs';
+
+const detectChangelog = function detectChangelog(pathname) {
+    const filePath = path.resolve(pathname, 'CHANGELOG.md');
+    let changelogExists = false;
+    try {
+        changelogExists = fs.accessSync(filePath, fs.R_OK | fs.W_OK);
+        changelogExists = true;
+    } catch (e) { }
+    return changelogExists;
+};
 
 export default function parser(dir = process.cwd()) {
-    const pathname = path.resolve(dir, 'CHANGELOG.md');
-    let contents = fs.readFileSync(pathname, 'utf8');
+
 
     return {
         remark,
-        root: remark.parse(contents),
+        root: {},
+        contents: '',
+        pathname: path.resolve(dir, 'CHANGELOG.md'),
+        parse() {
+            this.root = remark.parse(this.contents);
+        },
         add(message) {
             console.log('ADDED: ' + message);
         },
         stringify() {
             return this.remark.stringify(this.root);
         },
-        write() {
-            fs.writeFileSync(pathname, this.stringify());
+        exists() {
+            return detectChangelog(dir);
+        },
+        read() {
+            this.contents = fs.readFileSync(this.pathname, 'utf8');
+        },
+        write(data, userPath = this.pathname) {
+            fs.writeFileSync( userPath, data );
         }
     };
 }
