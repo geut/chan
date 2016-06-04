ecc7914b557c52e6c0ca3e680458e3646049e685
Diego Paez <paez.di@gmail.com>
2016-06-04 17:58:12 -0300

    Update: wip tests

--- test/commands/init.js
+++ test/commands/init.js
@@ -1,20 +1,59 @@
+import fs from 'fs';
+import * as child from 'child_process';
+import path from 'path';
 import test from 'tape';
-import proxyquire from 'tape';
-import cli from '../../src/cli';
+import bddStdin from 'bdd-stdin';
+import { stdin } from 'mock-stdin';
+
 // import tempfile from 'tempfile';
 
-// const stubs = {
-//   'inquirer': {
-//     prompt: new Prompt(this.fixture, this.rl),
-//     '@global': true
-//   }
-// };
+const binLoc = path.normalize(`${__dirname}/../../es5/cli/runner.js`);
 
-const originalArgv = process.argv;
-
-test('test "init" command', (t) => {
+test('test "init" command --> Precondition: CHANGELOG.md does not exists / Postcondition: command should create a new CHANGELOG.md file in the path: /tmp/chan_test', (t) => {
     t.plan(1);
-    process.argv.push( 'chan', '--path', '/tmp/chan_test/', 'init' );
-    cli.run();
-    t.pass('example');
+    const args = [binLoc];
+    args.push('--path', '/tmp/chan_test/', 'init' );
+    child.spawn(binLoc, args);
+
+    setTimeout( () => {
+        t.doesNotThrow( () => {
+            fs.openSync('/tmp/chan_test/CHANGELOG.md', 'r');
+        }, true, 'CHANGELOG.md successfully created.');
+    }, 1000 );
+});
+
+const getStats = function getStats() {
+    let stats = false;
+    try {
+        stats = fs.statSync( '/tmp/chan_test/CHANGELOG.md' );
+    } catch (e) {
+        stats = false;
+    }
+    return stats;
+};
+
+test('test "init" command --> Precondition: CHANGELOG.md exists / Postcondition: command should create a new CHANGELOG.md file in the path: /tmp/chan_test. Prompt interaction (user) is mocked.', (t) => {
+    t.plan(4);
+
+    // First check previous file exists and its mtime
+    const originalStats = getStats();
+    t.equal(typeof originalStats, 'object', 'CHANGELOG.md already exists, stats data obtained OK');
+
+    // Then run the init command.
+    const args = [binLoc];
+    // user answer to overwrite previous changelog
+    stdin().send('y');
+    stdin().send(null);
+    args.push('--path', '/tmp/chan_test/', 'init' );
+    child.spawn(binLoc, args);
+
+    setTimeout( () => {
+        t.doesNotThrow( () => {
+            fs.openSync('/tmp/chan_test/CHANGELOG.md', 'r');
+        }, true, 'CHANGELOG.md successfully created.');
+
+        const finalStats = getStats();
+        t.equal(typeof finalStats, 'object', 'CHANGELOG.md already exists, stats data obtained OK');
+        t.notEqual(originalStats.mtime, finalStats.mtime, 'Modification times should be different');
+    }, 1000 );
 });
