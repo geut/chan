6468085241c02f595cc0efd729f851580ea3f14d
Diego Paez <paez.di@gmail.com>
2016-06-05 19:48:30 -0300

    Update: wip added command

--- src/cli/commands/added.js
+++ src/cli/commands/added.js
@@ -1,9 +1,25 @@
+const addedHeaderTemplate = `
+Added
+
+`;
+
+/*const addedMessageTemplate = `
+    - ${msg = msg || ''}
+`;
+*/
 export default function () {
     return {
         command: 'added <msg>',
         describe: 'Writes your changelog indicating new stuff.',
         handler(parser, argv, write) {
-            console.log('ADDED: ' + argv.msg);
+            if ( !parser.exists() ) {
+                // maybe throw an error... init is needed first
+                return;
+            }
+
+            const msg = argv;
+            console.log(msg)
+            console.log(parser.root)
         }
     };
 }
