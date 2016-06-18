a782399e41b1288ca59fa5df48179259af437510
Diego Paez <paez.di@gmail.com>
2016-06-18 17:39:32 -0300

    Update: minor refactor

--- src/parser/index.js
+++ src/parser/index.js
@@ -3,11 +3,19 @@ import remark from 'remark';
 import inject from 'mdast-util-inject';
 import { read, write } from './fs';
 
+
+const MARKERS = {
+    INITIAL: 0,
+    UNRELEASED: 3,
+    CHANGES: 4
+};
+
 export default function parser(dir = process.cwd()) {
     const pathname = path.resolve(dir, 'CHANGELOG.md');
     const contents = read(pathname);
     return {
         remark,
+        MARKERS,
         root: remark.parse(contents),
         createMDAST(value) {
             const result = remark.parse(value);
@@ -26,17 +34,16 @@ export default function parser(dir = process.cwd()) {
             return this.remark.stringify(this.root);
         },
         addedHeaderExists() {
-            return this.root.children[3] &&
-                this.root.children[3].type === 'heading' &&
-                this.root.children[3].children[0] &&
-                this.root.children[3].children[0].value === 'Added';
+            return this.root.children[MARKERS.UNRELEASED] &&
+                this.root.children[MARKERS.UNRELEASED].type === 'heading' &&
+                this.root.children[MARKERS.UNRELEASED].depth === 3;
         },
         unreleased() {
-            if (!(this.root.children[4] && this.root.children[4].type === 'list')) {
+            if (!(this.root.children[MARKERS.CHANGES] && this.root.children[MARKERS.CHANGES].type === 'list')) {
                 // probably we should create it?
                 return false;
             }
-            return this.root.children[4];
+            return this.root.children[MARKERS.CHANGES];
         },
         added(value) {
             let result = false;
