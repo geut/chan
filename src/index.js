d785a593f24107ff70f4c3dc5f048407690a3a30
Max Fierro <elmasse@gmail.com>
2017-02-09 03:22:52 -0300

    renamed api -> chan

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import * as api from './api';
+import * as chan from './api';
 
 export { default as cli } from './cli';
 export { default as parser } from './parser';
-export default api;
+export default chan;
