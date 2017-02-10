52c619f7b1557a7b20ba5bc70714dc252b69466a
Max Fierro <elmasse@gmail.com>
2017-02-10 18:56:27 -0300

    remove commented code

--- src/cli/commands/init.js
+++ src/cli/commands/init.js
@@ -1,12 +1,3 @@
-// const initTemplate = `
-// # Change Log
-// All notable changes to this project will be documented in this file.
-
-// The format is based on [Keep a Changelog](http://keepachangelog.com/)
-// and this project adheres to [Semantic Versioning](http://semver.org/).
-
-// ## [Unreleased]
-// `;
 
 import init from '../../api/init';
 
