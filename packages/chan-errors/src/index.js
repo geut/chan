1320b4ca7fa1f7b63f8d7a9b3405bb1c39d0c61f
Esteban Primost <esteban.primost@gmail.com>
2018-12-05 16:10:56 -0300

    Errors: Changelog not exists error specification

--- packages/chan-errors/src/index.js
+++ packages/chan-errors/src/index.js
@@ -17,13 +17,28 @@ export class ChangelogAlreadyExistsError extends ChanError {
 }
 
 export class ChangelogNotExistsError extends ChanError {
+  constructor(info, extraMsg) {
+    super(`CHANGELOG.md does not exists.${extraMsg}`, info);
+  }
+}
+
+export class ChangelogNotExistsOnPathError extends ChangelogNotExistsError {
   constructor(info) {
-    super(`CHANGELOG.md does not exists. Path: ${info.path}.`, info);
+    super(`CHANGELOG.md does not exists.`, ` Path: ${info.path}.`, info);
   }
 }
 
 export class VersionAlreadyExistsError extends ChanError {
   constructor(info) {
     super(`Version ${info.version} already exists on CHANGELOG.md.`, info);
+  }
+}
+
+export class NoReaderProvidedError extends ChanError {
+  constructor(info) {
+    super(
+      `If no path is provided, you must provide a Reader = { read(), exists() } for get CHANGELOG contents.`,
+      info
+    );
   }
 }
